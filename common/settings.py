cafbe53b8ac3d07b7dcca90750e9d09d66310602
Luffy-lp <55189314+Luffy-lp@users.noreply.github.com>
2021-02-10 18:32:20 +0800

    一次提交

--- common/settings.py
+++
@@ -1,8 +0,0 @@
-#  日志相关的配置
-LOGGING = dict(
-    level='DEBUG',
-    fh_level="DEBUG",
-    sh_level="ERROR",
-    ih_level="INFO",
-    log_name="logging.log",
-)
--- pages/startgame/page_loginGuide.py
+++
@@ -1,1 +0,0 @@
-from common.COM_findobject import FindObject
